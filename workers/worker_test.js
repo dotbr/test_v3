554d98f91eb695c913ac58998d029c359ca3c193
Leandro Ramalho <leandroramalho@gmail.com>
2015-03-30 13:39:36 -0300

    better logging

--- workers/worker_test.js
+++ workers/worker_test.js
@@ -2,38 +2,39 @@ var jackrabbit = require('jackrabbit');
 var mongo = require('mongodb');
 
 // Connection URL
-var url = 'mongodb://localhost:27017/test';
-// Use connect method to connect to the Server
-var mongoDB;
-var queue;
+var mongoUrl = 'mongodb://localhost:27017/test';
+var rabbitUrl = 'amqp://localhost';
+var rabbitQueue = 'jobs.add';
 
-mongo.connect(url, function(err, db) {
+mongo.connect(mongoUrl, function(err, db) {
   if (err)
   {
     console.log("Error connecting to MongoDB: " + err);
     process.exit(1);
   }
 
-  mongoDB = db;
-  console.log("Connected to MongoDB: " + url);
-  queue = jackrabbit('amqp://localhost');
+  console.log("Connected to MongoDB: " + mongoUrl);
+  var queue = jackrabbit(rabbitUrl);
 
   queue.on('connected', function() {
-    queue.create('jobs.add', { }, onReady);
+    console.log("Connected to rabbitMQ: " + rabbitUrl);
+
+    queue.create(rabbitQueue, { }, onReady);
 
     function onReady() {
-      queue.handle('jobs.add', onJob);
+      console.log("Listening to " + rabbitQueue);
+      queue.handle(rabbitQueue, onJob);
     }
 
     function onJob(job, ack) {
-      console.log('Inserting ' + job.toString());
+      console.log('Got message: ' + job.toString());
       var obj = JSON.parse(job);
-      var objID = insertDocument(obj, mongoDB, function(err, result) {
+      var objID = insertDocument(obj, db, function(err, result) {
         if (err) { console.log("Error inserting document: " + err); return;}
         var objID = obj._id.toHexString();
         console.log('Inserted ' + result + ", objID= " + objID);
-        console.log("Sending ack: " + objID);
         ack(objID);
+        console.log("Ack sent: " + objID);
       });
       //process.exit();
     };
